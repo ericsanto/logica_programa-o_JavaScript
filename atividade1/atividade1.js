8bced17ec1a12eaf87de76014efb4f1e80fd45fd
Eric <ericjesus403@gmail.com>
2024-03-17 14:57:13 -0300

    Atividade 1

---
+++ atividade1/atividade1.js
@@ -0,0 +1,8 @@
+let firstName =  prompt('Digite seu nome: ');
+let secondName = prompt('Digite seu sobrenome: ');
+let fullName = firstName + " " + secondName;
+
+alert(
+  "Seu nome completo é " + " " + fullName
+  
+);
