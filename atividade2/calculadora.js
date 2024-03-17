070d8cbd179e942251e9b9e6cfcee05020da5fd2
Eric <ericjesus403@gmail.com>
2024-03-17 14:57:53 -0300

    Utilizando operadores +-*/

---
+++ atividade2/calculadora.js
@@ -0,0 +1,16 @@
+const x = parseFloat(prompt('Digite um número: '))
+
+const y = parseFloat(prompt('Digite mais um número: '))
+
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert(
+  "Resultado:\n " +
+  "\nSoma: " + soma +
+  "\nA subtração é: " +  subtracao +
+  "\nA multiplicação é: " + multiplicacao +
+  "\nA divisão é: " + divisao 
+)
